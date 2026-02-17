c1c1a1e4c4bfac09a2e1deae3427365255804fed
drestrep <davidro112014@gmail.com>
2026-02-17 12:15:26 +0100

    More than one user can join a channel now, and operators are visible in HexChat.
    Updated TODO list.

--- Client.hpp
+++ Client.hpp
@@ -2,19 +2,21 @@
 #define CLIENT_HPP
 
 #include <string>
+#include <set>
 
 class Client {
 public:
     Client();
     Client(int fd);
     ~Client();
+	std::set<std::string> channels_operating;
     bool _isRegistered;
     bool _isPasswordOk;
     bool isRegisted() const;
     void setRegisted(bool value);
     int getFd() const;
     std::string &getBuffer();
-    
+
     void setNickname(const std::string &nickname);
     void setUsername(const std::string &username);
     std::string &getNickname();
--- Server.cpp
+++ Server.cpp
@@ -285,10 +285,17 @@ void Server::executeCommand(int fd, const std::vector<std::string>& args) {
                 return;
             }
             std::string chanName = args[1];
+            
+            // Añadir # al nombre del canal si no lo tiene
+            if (chanName[0] != '#') {
+                chanName = "#" + chanName;
+            }
+            
             std::map<std::string, Channel>::iterator it = _channels.find(chanName);
             if (it == _channels.end()) {
                 _channels.insert(std::make_pair(chanName, Channel(chanName)));
                 it = _channels.find(chanName);
+				user.channels_operating.insert(chanName);
             }
             Channel& channel = it->second;
             if (channel.hasUser(&user)) {
@@ -305,21 +312,27 @@ void Server::executeCommand(int fd, const std::vector<std::string>& args) {
             std::string joinMsg = prefix + " JOIN :" + chanName;
             sendToChannel(channel, joinMsg);
 
-            // Send NAMES reply (353) and end of NAMES (366) to the joining user
             // Build user list
             std::string userList;
             const std::set<Client*>& users = channel.getUsers();
             for (std::set<Client*>::const_iterator it2 = users.begin(); it2 != users.end(); ++it2) {
                 if (!userList.empty()) userList += " ";
+				// Añadir @ para operadores
+				if (channel.isOperator(*it2))
+					userList += "@";
                 userList += (*it2)->getNickname();
             }
-            std::string nick = user.getNickname();
-            std::string namesReply = ":" + serverName + " 353 " + nick + " = " + chanName + " :" + userList;
-            std::cout << "Sending: " << namesReply << std::endl;
-			send_message(fd, namesReply);
-            std::string endNames = ":" + serverName + " 366 " + nick + " " + chanName + " :End of /NAMES list.";
-            std::cout << "Sending: " << endNames << std::endl;
-			send_message(fd, endNames);
+            
+            // Enviar NAMES a todos los usuarios del canal para mostrar la lista actualizada
+            for (std::set<Client*>::const_iterator it2 = users.begin(); it2 != users.end(); ++it2) {
+                Client* c = *it2;
+                std::string namesReply = ":" + serverName + " 353 " + c->getNickname() + " = " + chanName + " :" + userList;
+                std::cout << "Sending NAMES to " << c->getNickname() << ": " << namesReply << std::endl;
+                send_message(c->getFd(), namesReply);
+                std::string endNames = ":" + serverName + " 366 " + c->getNickname() + " " + chanName + " :End of /NAMES list.";
+                std::cout << "Sending: " << endNames << std::endl;
+                send_message(c->getFd(), endNames);
+            }
             break;
         }
         //--------- PRIVMSG -----------
