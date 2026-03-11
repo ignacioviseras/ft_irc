1ae7f46633c2af0aeb1e02e56264e30302a7d716
David <drestrep@student.42madrid.com>
2026-03-11 13:53:13 +0100

    Fixed pass bug with more than 2 arguments.

--- src/commands/pass.cpp
+++ src/commands/pass.cpp
@@ -2,14 +2,19 @@
 
 void Server::_pass(Client* sender, const std::vector<std::string>& args) {
     int fd = sender->getFd();
-    if (args.size() < 2) {
-        std::string errorMsg = ":irc.servidor.com 461 " + sender->getNickname() + " PASS :Not enough parameters";
-        send_message(fd, errorMsg);
-        return;
-    }
     if (sender->isRegisted()) {
         std::string errorMsg = ":irc.servidor.com 462 " + sender->getNickname() + " :Unauthorized command (already registered)";
         return send_message(fd, errorMsg);
+    }
+    if (args.size() != 2) {
+        std::string errorMsg;
+        if (args.size() < 2) {
+            errorMsg = ":irc.servidor.com 461 " + sender->getNickname() + " PASS :Not enough parameters";
+        } else {
+            errorMsg = ":irc.servidor.com 461 " + sender->getNickname() + " PASS :Too many parameters";
+        }
+        send_message(fd, errorMsg);
+        return;
     }
     if (args[1] == this->_password) {
         sender->_isPasswordOk = true;
