4edcb673545a313c98b19308d411e9bfe985c813
drestrep <davidro112014@gmail.com>
2026-03-12 18:16:31 +0100

    Kick now works with more than one user on the same command.
    Updated TODO.

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -1,34 +1,97 @@
 #include "../../include/Server.hpp"
 
 void	Server::_kick(Client* sender, const std::vector<std::string>& args) {
+
 	if (args.size() < 3) {
 		std::string errorMsg = ":irc.servidor.com 461 " + sender->getNickname() + " KICK :Not enough parameters";
 		send_message(sender->getFd(), errorMsg);
 		return;
 	}
-	std::string chanName = args[1];
-	std::string targetNick = args[2];
 
-	//std::cout << "HA ENTRADO EN KICK CON CANAL: " << chanName << " Y TARGET: " << targetNick << std::endl;
+	const std::string chanName = args[1];
 	std::map<std::string, Channel>::iterator it = _channels.find(chanName);
 	if (it == _channels.end()) {
 		std::string errorMsg = ":irc.servidor.com 403 " + sender->getNickname() + " " + chanName + " :No such channel";
 		send_message(sender->getFd(), errorMsg);
-		//send_message(sender->getFd(), "Error: El canal no existe.");
-		return;
+		return ;
 	}
+
 	Channel& channel = it->second;
+	
+	if (!channel.hasUser(sender)) {
+		std::string errorMsg = ":irc.servidor.com 442 " + sender->getNickname() + " " + chanName + " :You're not on that channel";
+		send_message(sender->getFd(), errorMsg);
+		return ;
+	}
+
 	if (!channel.isOperator(sender)) {
 		std::string errorMsg = ":irc.servidor.com 482 " + sender->getNickname() + " " + chanName + " :You must be a channel operator";
 		send_message(sender->getFd(), errorMsg);
-		return;
+		return ;
 	}
+
+	// Build comment (default to sender's nickname as per spec)
+    std::string comment = (args.size() >= 4) ? args[3] : sender->getNickname();
+
+    // Parse comma-separated target users from args[2]
+    std::vector<std::string> targetNicks;
+    std::string targetList = args[2];
+    std::string::size_type start = 0;
+    std::string::size_type pos = targetList.find(',');
+    while (pos != std::string::npos) {
+        std::string nick = targetList.substr(start, pos - start);
+        if (!nick.empty())
+            targetNicks.push_back(nick);
+        start = pos + 1;
+        pos = targetList.find(',', start);
+    }
+    std::string lastNick = targetList.substr(start);
+    if (!lastNick.empty())
+        targetNicks.push_back(lastNick);
+
+    for (size_t i = 0; i < targetNicks.size(); ++i) {
+        const std::string& targetNick = targetNicks[i];
+
+        Client* target = findClientByNick(targetNick);
+        if (!target || !channel.hasUser(target)) {
+            send_message(sender->getFd(), ":irc.servidor.com 441 " + sender->getNickname() + " " + targetNick + " " + chanName + " :They aren't on that channel");
+            continue;
+        }
+
+        // Spec: one KICK message per user, with comment
+        std::string kickMsg = ":" + sender->getNickname() + "!" + sender->getUsername() + "@irc.servidor.com KICK " + chanName + " " + targetNick + " :" + comment;
+        sendToChannel(channel, kickMsg);
+
+        channel.removeUser(target);
+
+        // Re-check if channel still exists after removal
+        if (channel.getUsers().empty()) {
+            _channels.erase(it);
+            break;
+        }
+    }
+}
+	/* std::set<std::string> targetNicks;
+	for (size_t i = 2; i < args.size(); ++i) {
+		targetNicks.insert(args[i]);
+	}
+
+	std::set<Client*> targets;
+	for (std::set<std::string>::const_iterator it = targetNicks.begin(); it != targetNicks.end(); ++it) {
+		Client* target = findClientByNick(*it);
+		if (target) {
+			targets.insert(target);
+		} else {
+			std::string errorMsg = ":irc.servidor.com 441 " + sender->getNickname() + " " + *it + " " + chanName + " :They aren't on that channel";
+			send_message(sender->getFd(), errorMsg);
+		}
+	}
+
+	std::map<std::string, Channel>::iterator it = _channels.find(chanName);
+
 	Client* target = findClientByNick(targetNick);
-	if (!target || !channel.hasUser(target)) {
-		std::string errorMsg = ":irc.servidor.com 441 " + sender->getNickname() + " " + targetNick + " " + chanName + " :They aren't on that channel";
-		send_message(sender->getFd(), errorMsg);
-		return;
-	}
+	std::map<std::string, Channel>::iterator it = _channels.find(chanName);
+	
 	// Enviar KICK a todos (incluyendo al expulsado) ANTES de removerlo
 	std::string kickMsg = ":" + sender->getNickname() + "!" + sender->getUsername() + "@irc.servidor.com KICK " + chanName + " " + targetNick;
 	sendToChannel(channel, kickMsg);
@@ -39,3 +102,4 @@ void	Server::_kick(Client* sender, const std::vector<std::string>& args) {
 		_channels.erase(it);
 	}
 }
+ */
